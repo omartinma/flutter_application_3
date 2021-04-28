1744047e7fc6662630dba41e1a3e728eb8284481
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-04-29 01:03:21 +0200

    update scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool isHello = true;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+      ),
     );
   }
 }
