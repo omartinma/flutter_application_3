05de720cd079d8d199f2354fc7afd609acebe289
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-05-04 19:15:28 +0200

    fix scroll

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,7 +14,7 @@ class HomePage extends StatelessWidget {
           onPressed: () {
             showModalBottomSheet(
               context: context,
-              isScrollControlled: false,
+              isScrollControlled: true,
               backgroundColor: Colors.transparent,
               builder: (context) => ExampleBottomSheet(),
             );
